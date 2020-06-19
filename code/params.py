dbd138f71a53a83668ab040e549798f0d41dd150
Luuk Jacobs <l.f.p.jacobs@student.tue.nl>
2020-06-19 12:51:51 +0200

    Add comments

--- code/params.py
+++ code/params.py
@@ -1,14 +1,14 @@
 """
-Define the parameters for main.py
+Define the parameters for the experiments (model parameters can be changed in main.py)
 """
 
-loadDirectory = "/data/tract2020/HCP_bundle_mapping/" # Folder with all subjects
+loadDirectory = "/data/tract2020/HCP_bundle_mapping/" # Folder with all subjects (HCP)
 saveDirectory = "/data/lfpjacobs"                     # Folder to which to save data, results, and model checkpoints
 
-bundle = "CST_left" # BOI
+bundle = "CST_left" # Bundle of interest (BOI)
 n = 100             # Number of points on tracts
 r_neighbor = 0.5    # Representation of neighboring tracts during training
-n_center = 10       # Number of centers of mass in BOI
+n_center = 10       # Number of centers of mass in BOI used to define neighboring region
 
 train = 6   # Number of training subjects
 test = 5    # Number of testing subjects
@@ -16,8 +16,8 @@ val = 2     # Number of validation subjects
 pseudo = 5  # Number of pseudo testing subjects for FP mining
 n_tot = 105 # Total number of subjects
 
-save = False    # Save the data for later runs
-load = True     # Load from saved data from previous run 
-t_SNE = False   # Save t-SNE embedding of the best epoch of the first validation subject
-SVM = False     # Test the effect of an SVM on top of the network
-FPMining = True # Test the effect of FP mining 
+save = False     # Save the loaded data for later runs
+load = True      # Load from saved data from previous run 
+t_SNE = False    # Save t-SNE embedding of the best epoch of the first validation subject
+SVM = False      # Test the effect of an SVM on top of the network
+FPMining = False # Test the effect of FP mining 
